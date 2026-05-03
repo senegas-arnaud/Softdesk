3ac4853c2b2877d199e92f15788a724953ff174e
Arnaud <arnaud.senegas@hotmail.com>
2026-05-03 17:46:24 +0200

    make project app

---
+++ softdesk/projects/views.py
@@ -0,0 +1,128 @@
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from .models import Project, Contributor, Issue, Comment
+from .serializers import ProjectSerializer, ContributorSerializer, IssueSerializer, CommentSerializer
+
+
+class ProjectListCreateView(generics.ListCreateAPIView):
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(contributors__user=self.request.user)
+
+    def perform_create(self, serializer):
+        project = serializer.save(author=self.request.user)
+        Contributor.objects.create(user=self.request.user, project=project)
+
+
+class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(contributors__user=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        project = self.get_object()
+        if project.author != request.user:
+            return Response({"detail": "Vous n'êtes pas l'auteur de ce projet."}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        project = self.get_object()
+        if project.author != request.user:
+            return Response({"detail": "Vous n'êtes pas l'auteur de ce projet."}, status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
+    
+    
+class ContributorListCreateView(generics.ListCreateAPIView):
+    serializer_class = ContributorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Contributor.objects.filter(project_id=self.kwargs['project_pk'])
+
+    def perform_create(self, serializer):
+        project = Project.objects.get(pk=self.kwargs['project_pk'])
+        if project.author != self.request.user:
+            raise permissions.PermissionDenied("Seul l'auteur peut ajouter des contributeurs.")
+        serializer.save(project=project)
+
+
+class ContributorDestroyView(generics.DestroyAPIView):
+    serializer_class = ContributorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Contributor.objects.filter(project_id=self.kwargs['project_pk'])
+
+    def destroy(self, request, *args, **kwargs):
+        project = Project.objects.get(pk=self.kwargs['project_pk'])
+        if project.author != request.user:
+            raise permissions.PermissionDenied("Seul l'auteur peut supprimer des contributeurs.")
+        return super().destroy(request, *args, **kwargs)
+
+
+class IssueListCreateView(generics.ListCreateAPIView):
+    serializer_class = IssueSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Issue.objects.filter(project_id=self.kwargs['project_pk'])
+
+    def perform_create(self, serializer):
+        project = Project.objects.get(pk=self.kwargs['project_pk'])
+        serializer.save(author=self.request.user, project=project)
+
+
+class IssueDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = IssueSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Issue.objects.filter(project_id=self.kwargs['project_pk'])
+
+    def update(self, request, *args, **kwargs):
+        issue = self.get_object()
+        if issue.author != request.user:
+            return Response({"detail": "Vous n'êtes pas l'auteur de cette issue."}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        issue = self.get_object()
+        if issue.author != request.user:
+            return Response({"detail": "Vous n'êtes pas l'auteur de cette issue."}, status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
+
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Comment.objects.filter(issue_id=self.kwargs['issue_pk'])
+
+    def perform_create(self, serializer):
+        issue = Issue.objects.get(pk=self.kwargs['issue_pk'])
+        serializer.save(author=self.request.user, issue=issue)
+
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Comment.objects.filter(issue_id=self.kwargs['issue_pk'])
+
+    def update(self, request, *args, **kwargs):
+        comment = self.get_object()
+        if comment.author != request.user:
+            return Response({"detail": "Vous n'êtes pas l'auteur de ce commentaire."}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        comment = self.get_object()
+        if comment.author != request.user:
+            return Response({"detail": "Vous n'êtes pas l'auteur de ce commentaire."}, status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
--- softdesk/softdesk/urls.py
+++ softdesk/softdesk/urls.py
@@ -2,11 +2,26 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import RegisterView, UserDetailView
+from projects.views import ProjectListCreateView, ProjectDetailView, ContributorListCreateView, ContributorDestroyView, IssueListCreateView, IssueDetailView,CommentListCreateView,CommentDetailView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Users
     path('api/users/register/', RegisterView.as_view(), name='register'),
     path('api/users/login/', TokenObtainPairView.as_view(), name='login'),
     path('api/users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/users/me/', UserDetailView.as_view(), name='user-detail'),
+    # Projects
+    path('api/projects/', ProjectListCreateView.as_view(), name='project-list-create'),
+    path('api/projects/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    # Contributors
+    path('api/projects/<int:project_pk>/contributors/', ContributorListCreateView.as_view(), name='contributor-list-create'),
+    path('api/projects/<int:project_pk>/contributors/<int:pk>/', ContributorDestroyView.as_view(), name='contributor-destroy'),
+    # Issues
+    path('api/projects/<int:project_pk>/issues/', IssueListCreateView.as_view(), name='issue-list-create'),
+    path('api/projects/<int:project_pk>/issues/<int:pk>/', IssueDetailView.as_view(), name='issue-detail'),
+    # Comments
+    path('api/projects/<int:project_pk>/issues/<int:issue_pk>/comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    path('api/projects/<int:project_pk>/issues/<int:issue_pk>/comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
 ]
